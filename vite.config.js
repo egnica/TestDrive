b26fe0f17ff4dd612235fda3032dc15a1dc6ba33
egnica <46502133+egnica@users.noreply.github.com>
2024-06-12 09:47:59 -0500

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
+import php from "vite-plugin-php";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "https://www.barlowresearch.com/mybarlow/TestDrive_Demo_2024/",
-	plugins: [react()],
+	base: "https://www.barlowresearch.com/mybarlow/testdrive2024/",
+	plugins: [react(), php()],
 });
